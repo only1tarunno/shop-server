9daa18154f11b0b21e673e25384a6246028a29c4
only1tarunno <tarunno281@gmail.com>
2023-12-03 17:23:27 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const applyMiddleWares = require("./middleWares/applyMiddleWares");
+const connectDB = require("./db/connectDb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// routers
+const authRoutes = require("./routes/Authentication/authentication");
+
+// middlewares
+applyMiddleWares(app);
+
+// jwt related api
+app.use(authRoutes);
+
+// allUsers api
+// app.use(allUsers);
+
+// server health check api
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+// handling all (get,post,update,delete.....) unhandled routes
+app.all("*", (req, res, next) => {
+  const error = new Error(`Can't find [${req.originalUrl}] on the server`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
+module.exports = app;
+// text
