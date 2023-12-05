e53c7219c30a3455105f7cb1ccb4c9c30d170543
only1tarunno <tarunno281@gmail.com>
2023-12-05 23:54:59 +0600

    review collection add

--- src/api/ProductsController/getSingleProduct.js
+++ src/api/ProductsController/getSingleProduct.js
@@ -1,10 +1,12 @@
 const Product = require("../../models/Product");
+const Review = require("../../models/Review");
 
 const getSingleProduct = async (req, res) => {
   const id = req.params.id;
   try {
-    const result = await Product.findById(id);
-    res.send(result);
+    const product = await Product.findById(id);
+    const reviews = await Review.find({ product_id: id });
+    res.send({ product, reviews });
   } catch (error) {
     res.send({ message: error.message });
   }
---
+++ src/models/Review.js
@@ -0,0 +1,33 @@
+const { model, Schema } = require("mongoose");
+
+const ReviewSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+  product_id: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+});
+
+const Review = model("Review", ReviewSchema);
+
+module.exports = Review;
