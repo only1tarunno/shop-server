6e068f6b5b4c5ded0a0f6be6460ce142b891b64f
only1tarunno <tarunno281@gmail.com>
2023-12-06 13:52:37 +0600

    fix quantity

--- src/api/cartController/createCartController.js
+++ src/api/cartController/createCartController.js
@@ -3,8 +3,21 @@ const Cart = require("../../models/Cart");
 const createCartController = async (req, res) => {
   try {
     const cartItem = req.body;
-    const result = await Cart.create(cartItem);
-    res.send(result);
+    console.log(cartItem.menuId);
+    const existingCartItem = await Cart.findOne({ menuId: cartItem.menuId });
+    if (existingCartItem) {
+      // If the cart item with the same menuId exists, update the quantity
+      existingCartItem.quantity += cartItem.quantity;
+      existingCartItem.totalPrice = (
+        existingCartItem.totalPrice + cartItem.totalPrice
+      ).toFixed(2);
+      const result = await existingCartItem.save();
+      res.send(result);
+    } else {
+      // If the cart item doesn't exist, create a new one
+      const result = await Cart.create(cartItem);
+      res.send(result);
+    }
   } catch (error) {
     res.send({ message: error.message });
   }
