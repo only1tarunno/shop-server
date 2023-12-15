eea2db99485506a9590d707b976c04f42d3ee91c
only1tarunno <tarunno281@gmail.com>
2023-12-15 19:08:10 +0600

    fix bug

--- src/api/cartController/getCartProducts.js
+++ src/api/cartController/getCartProducts.js
@@ -4,24 +4,10 @@ const getCartProducts = async (req, res) => {
   const email = req.query.email;
 
   try {
-    const totalQuantity = await Cart.aggregate([
-      { $match: { email } }, // Match documents for the given email
-      {
-        $group: {
-          _id: null,
-          totalQuantity: { $sum: "$quantity" }, // Calculate the sum of quantities
-        },
-      },
-    ]);
-
     const cartProducts = await Cart.find({ email }); // Fetch cart products
 
     // Send both cart products and total quantity in the response
-    res.send({
-      cartProducts,
-      totalQuantity:
-        totalQuantity.length > 0 ? totalQuantity[0].totalQuantity : 0,
-    });
+    res.send(cartProducts);
   } catch (error) {
     res.send({ message: error.message });
   }
